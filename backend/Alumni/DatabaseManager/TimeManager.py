569c63f8402674eb14ed3e6cc188a2fa82221eac
Starrah <46821571+Starrah@users.noreply.github.com>
2019-12-18 01:23:05 +0800

    fix: 模版消息参数bug和长度bug

--- backend/Alumni/DatabaseManager/TimeManager.py
+++ backend/Alumni/DatabaseManager/TimeManager.py
@@ -165,18 +165,17 @@ def SendTimedMessage(TheActivityItem, TheUserItem, TheMessageType):
                     TheMessageType == Constants.MESSAGE_TYPE_ACTIVITY_FORBIDDEN):
                 obj["template_id"] = "LEoLo9UsF2by_4UGypKf2v7YLXeYRRGGjskOa0iJzZY"
                 data["thing2"] = {"value": TheActivityItem.Name}
-                if (TheMessageType == Constants.MESSAGE_TYPE_ACTIVITY_CHANGE):
-                    data["date4"] = {"value": time.strftime("%Y{y}%m{m}%d{d} %H:%M", time.localtime(TheActivityItem.StartTime)).format(y='年', m='月', d='日')}
-                    data["thing5"] = {"value": TheActivityItem.Place}
+                data["date4"] = {"value": time.strftime("%Y{y}%m{m}%d{d} %H:%M", time.localtime(TheActivityItem.StartTime)).format(y='年', m='月', d='日')}
+                data["thing5"] = {"value": TheActivityItem.Place}
                 detail = None
                 if (TheMessageType == Constants.MESSAGE_TYPE_KICK):
-                    detail = "您被活动管理员踢出了该活动。如有问题请与活动主办方联系。"
+                    detail = "您被活动主办方踢出了该活动。"
                 elif (TheMessageType == Constants.MESSAGE_TYPE_ACTIVITY_CANCEL):
-                    detail = "活动主办方取消了该活动，请您知悉。如有问题请与活动主办方联系。"
+                    detail = "活动主办方取消了该活动。"
                 elif (TheMessageType == Constants.MESSAGE_TYPE_ACTIVITY_FORBIDDEN):
-                    detail = "由于可能存在违反国家法律法规及政策、校友会章程、平台运营规则等行为，该活动已被封禁，请您知悉。"
+                    detail = "由于违规行为，该活动已被封禁。"
                 elif (TheMessageType == Constants.MESSAGE_TYPE_ACTIVITY_CHANGE):
-                    detail = "活动安排发生了变更，请您知悉。点击可查看活动详细信息。"
+                    detail = "活动安排发生了变更。点击可查看详情。"
                 data["thing6"] = {"value": detail}
             elif (TheMessageType == Constants.MESSAGE_TYPE_AUDIT_PASS or TheMessageType == Constants.MESSAGE_TYPE_AUDIT_FAIL):
                 obj["template_id"] = "j9EPrZx9MAQ5SjbN1aCYHImxymn6ZEziJLgQEcbuXSk"
@@ -351,5 +350,5 @@ def ChangeActivityByTime(request):
 
 def Testt(request):
     ChangeActivityByTime(None)
-    SendTimedMessageUser("10", "o9H2m5NRduLhddc_e-npjO2uBkTk", Constants.MESSAGE_TYPE_ACTIVITY_WILL_START_MINUTE)
+    SendTimedMessageUser("10", "o9H2m5NRduLhddc_e-npjO2uBkTk", Constants.MESSAGE_TYPE_ACTIVITY_CANCEL)
     return JsonResponse({})
